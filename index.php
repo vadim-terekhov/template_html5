930fa8b772431afd2b800e58d3fdb17398d0a1cd
vadim <kalambus-22@mail.ru>
2018-06-26 22:33:12 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta lang="ru">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<title>Оборудование для майнинга</title>
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.min.css">
+</head>
+<body>
+
+	<div id="osnova">
+		<section id="topmenu">
+			
+		</section>
+		<section></section>
+		<section></section>
+		
+	</div>>
+	
+
+
+
+	
+	
+	<!--      SCRIPTS     -->
+	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+	<script type="text/javascript" src="js/jquery-migrate-3.0.0.js"></script>
+	<script type="text/javascript" src="js/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="js/flowtype.js"></script>
+	<script type="text/javascript" src="js/wow.min.js"></script>
+	<script type="text/javascript" src="js/jquery.fancybox.min.js"></script>
+	<script type="text/javascript" src="js/my.js"></script>
+
+</body>
+</html>
